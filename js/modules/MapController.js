876e460fe7511f975e28daaa1ce98990209f725b
Arno Fiva <arnofiva@gmail.com>
2019-02-18 08:15:20 +0100

    Replace MapView with SceneView and added buildings from New York

--- js/modules/MapController.js
+++ js/modules/MapController.js
@@ -1,18 +1,20 @@
 define([
     "esri/Map",
-    "esri/views/MapView",
+    //"esri/views/MapView",
+    "esri/views/SceneView",
     "esri/layers/VectorTileLayer",
     "esri/views/layers/LayerView",
+    "esri/layers/Layer",
     "esri/widgets/Search",
     "modules/Utils",
     "dojo/domReady!"
-], function(Map, MapView, VectorTileLayer, LayerView, Search, Utils) {
+], function(Map, SceneView, VectorTileLayer, LayerView, Layer, Search, Utils) {
 
     //Constructor for a new MapController
     var MapController = function (viewDiv){
         this.viewDiv = viewDiv;
     }
-    
+
     //Builds the default map
     MapController.prototype.buildMap = function(){
         var that = this;
@@ -20,12 +22,31 @@ define([
         var item = "https://arcgis.com/sharing/rest/content/items/5ad3948260a147a993ef4865e3fad476";
         this.map = new Map();
 
-        this.view = new MapView({
+        this.view = new SceneView({
             container: viewDiv,
             map: this.map,
-            zoom: 13,
-            center: [-0.010557, 51.495997]
+            qualityProfile: "high",
+            environment: {
+              starsEnabled: false,
+              atmosphereEnabled: false,
+              lighting: {
+                directShadowsEnabled: false,
+                cameraTrackingEnabled: true,
+                ambientOcclusionEnabled: false
+              }
+            },
+            camera: {
+              position: {
+                spatialReference: { latestWkid: 3857, wkid: 102100 },
+                x: -8240232.016157305,
+                y: 4967521.10016462,
+                z: 542.5048153763637
+              },
+              heading: 60.806845587349486,
+              tilt: 70.00852977355764
+            }
         });
+        window.view = this.view;
 
         var searchWidget = new Search({
             view: this.view
@@ -40,13 +61,23 @@ define([
             url: item + "/resources/styles/root.json",
             opacity:1
         });
-        
+
         this.map.add(tileLyr);
+
+        Layer.fromPortalItem({
+          portalItem: {
+            id: "2e0761b9a4274b8db52c4bf34356911e"
+          }
+        }).then(layer => {
+          this.buildingLayer = layer;
+          this.map.add(layer);
+        });
 
         tileLyr.on("layerview-create", function (evt) {
             layerView = evt.layerView;
             //Set the original JSON style as a variable. We will need to revert to this each time we update the map's style
             that.originalStyle = JSON.stringify(layerView.layer.styleRepository.styleJSON);
+
             mapWait.resolve();
         });
         return mapWait.promise();
@@ -57,20 +88,46 @@ define([
         var array = Utils.getBaseColourArray();
         var style = this.originalStyle;
         var dict = Utils.getColourRamp(palette.colours[0], palette.colours[1]);
-         document.body.style.background = palette.colours[0];
+        document.body.style.background = palette.colours[0];
 
-        for (var i = 0; i <= array.length; i++) {
+        for (var i = 0; i < array.length; i++) {
             style = Utils.stringReplace(style, '"background-color":"' + array[i] + '"', '"background-color":"' + dict[array[i]] + '"')
             style = Utils.stringReplace(style, '"fill-color":"' + array[i] + '"', '"fill-color":"' + dict[array[i]] + '"');
             style = Utils.stringReplace(style, '"fill-outline-color":"' + array[i] + '"', '"fill-outline-color":"' + dict[array[i]] + '"');
             style = Utils.stringReplace(style, '"text-color":"' + array[i] + '"', '"text-color":"' + palette.colours[3] + '"');
             style = Utils.stringReplace(style, '"text-halo-color":"' + array[i] + '"', '"text-halo-color":"' + palette.colours[4] + '"');
             style = Utils.stringReplace(style, '"line-color":"' + array[i] + '"', '"line-color":"' + palette.colours[2] + '"');
+
         }
         var newStyle = JSON.parse(style);
         palette.storeStyle(style);
 
         this.map.layers.items[0].loadStyle(newStyle);
+
+        this.buildingLayer.renderer = {
+          type: "simple",
+          symbol: {
+            type: "mesh-3d",
+            symbolLayers: [
+              {
+                type: "fill",
+                material: {
+                  color: dict[array[12]],
+                  colorMixMode: "replace"
+                },
+                edges: {
+                  type: "solid",
+                  color: palette.colours[2],
+                  size: 0.6
+                }
+              }
+            ]
+          }
+        };
+        this.view.environment.background = {
+          type: "color",
+          color: palette.colours[2],
+        };
     }
 
     MapController.prototype.hideMap = function(){
@@ -83,6 +140,6 @@ define([
     //Stuff to make public
     return {
         MapController: MapController
-        
+
     };
 });
